bad390b6a405418a06d6031c06343c48135065ca
hackerrank <me@hackerrank.com>
2022-06-15 12:52:08 -0400

    ratings added

--- app.py
+++ app.py
@@ -1,51 +1,29 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
+import googlemaps
+import os
+from dotenv import load_dotenv
 app = Flask(__name__)
 
+load_dotenv()
 
-@app.route('/getmsg/', methods=['GET'])
+@app.route('/ratings', methods=['GET'])
 def respond():
-    # Retrieve the name from the url parameter /getmsg/?name=
-    name = request.args.get("name", None)
+    
+    gmaps = googlemaps.Client(key=os.getenv("GOOGLE_API_KEY"))
 
-    # For debugging
-    print(f"Received: {name}")
+    place = gmaps.place('ChIJDRGiQA39zUwRHzX2ExFiRG0')
+    ratings = place['result']['rating']
+    total_ratings = place['result']['user_ratings_total']
 
-    response = {}
-
-    # Check if the user sent a name at all
-    if not name:
-        response["ERROR"] = "No name found. Please send a name."
-    # Check if the user entered a number
-    elif str(name).isdigit():
-        response["ERROR"] = "The name can't be numeric. Please send a string."
-    else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome API!"
-
-    # Return the response in json format
-    return jsonify(response)
+    return render_template('ratings.html', ratings=ratings, total_ratings=total_ratings)
 
 
-@app.route('/post/', methods=['POST'])
-def post_something():
-    param = request.form.get('name')
-    print(param)
-    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-    if param:
-        return jsonify({
-            "Message": f"Welcome {name} to our awesome API!",
-            # Add this option to distinct the POST request
-            "METHOD": "POST"
-        })
-    else:
-        return jsonify({
-            "ERROR": "No name found. Please send a name."
-        })
 
 
 @app.route('/')
 def index():
     # A welcome message to test our server
-    return "<h1>Welcome to our medium-greeting-api!</h1>"
+    return "<h1>WowMat Store API</h1>"
 
 
 if __name__ == '__main__':
