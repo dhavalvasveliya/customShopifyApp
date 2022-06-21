b0b5e492c87e6292641c45e8aa97f83f54c1cd08
hackerrank <me@hackerrank.com>
2022-06-21 16:30:43 -0400

    changed app.js

--- static/app.js
+++ static/app.js
@@ -1,10 +1,7 @@
-fetch('https://cors-anywhere.herokuapp.com/https://wowmat-heroku-app.herokuapp.com/ratings')
-  .then(response => response.json())
-    .then(data => {
-        document.getElementById('rating').innerHTML += `<span class="rating__stars" style="--rating: 5;"></span>
+
+document.getElementById('rating').innerHTML += `<span class="rating__stars" style="--rating: 5;"></span>
         <div class="rating__number">
-          <span class="rating__score">${data["rating"]}</span>
-          <div class="rating__reviews">${data["totalratings"]}</div>
+          <span class="rating__score">5</span>
+          <div class="rating__reviews">82</div>
         </div>`;
-    });
 
